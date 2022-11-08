f8eb227ac8a92b0fd1e924c4f6b8a3fa37a4414b
SooyAldahir <aldahir.ballina@ulv.edu.mx>
2022-11-07 18:37:29 -0600

    Agregar archivos de proyecto.

---
+++ anio bisiesto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anio_bisiesto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int año;
+
+            Console.WriteLine("ingrese el año que desea consultar");
+            año = int.Parse(Console.ReadLine());
+
+            if ( año % 4 == 0 && año % 100 != 0)
+            {
+                Console.WriteLine("{0}, es un año bisiesto", año);
+            }
+            else if (año % 400 == 0 )
+            {
+                Console.WriteLine("{0}, es un año bisiesto", año);
+            }
+            else
+            {
+                Console.WriteLine("{0}. no es un año bisiesto", año);
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
